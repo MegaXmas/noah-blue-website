722d0480849c87ca3d2f813644c01183b7f86bf8
MegaXmas <noahmendelsohn96@gmail.com>
2025-10-16 10:51:58 -0700

    started working on songs i like ts

--- src/app/components/stuff-i-like/songs-i-like/songs-i-like.ts
+++ src/app/components/stuff-i-like/songs-i-like/songs-i-like.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal, OnInit, OnDestroy} from '@angular/core'
+
+import { FavoriteSong } from '../../../models/favorite-song';
+
+import { Marquee } from '../../marquee/marquee';
+import { FavoriteStuff } from '../../../models/favorite-stuff';
 
 @Component({
   selector: 'app-songs-i-like',
-  imports: [],
+  imports: [Marquee],
   templateUrl: './songs-i-like.html',
   styleUrl: './songs-i-like.css'
 })
-export class SongsILike {}
+export class SongsILike implements OnInit, OnDestroy {
+
+  songArray: FavoriteSong[] = [
+    { songTitle: "",
+      songArtist: "", songAlbumTitle: "", songAlbumCover: "",
+      songLink: "",
+      songReview: "",
+      },
+    { songTitle: "",
+      songArtist: "", songAlbumTitle: "", songAlbumCover: "",
+      songLink: "",
+      songReview: "",
+      },
+    { songTitle: "",
+      songArtist: "", songAlbumTitle: "", songAlbumCover: "",
+      songLink: "",
+      songReview: "",
+      },
+    { songTitle: "",
+      songArtist: "", songAlbumTitle: "", songAlbumCover: "",
+      songLink: "",
+      songReview: "",
+      },
+  ]
+
+
+
+
+  ngOnInit(): void {
+    console.log("SONGSILIKECOMPONENT: " + this.songArray.length + " songs displayed")
+    console.log("SONGsILIKECOMPONENT: " + JSON.stringify(this.songArray))
+  }
+
+  ngOnDestroy(): void {
+      
+    console.log("leaving songs-i-like page")
+  }
+}
