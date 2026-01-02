47e4f8c9b8f7cbfc20295003a7b40345b3717037
MegaXmas <noahmendelsohn96@gmail.com>
2026-01-02 14:02:08 -0800

    doing @media fine tuning and fixing html @if statements

--- src/app/pipes/indent-after-first.pipe.ts
+++ src/app/pipes/indent-after-first.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class IndentAfterFirstPipe implements PipeTransform {
     transform(value: string): string {
-        const firstLineIndented = `&#xa0` + value;
+        const firstLineIndented = `&#xa0 ` + value;
         return firstLineIndented.replaceAll(`\n`, `\n&#xa0&#xa0&#xa0 `);
     }
 }
