940cccb3894b1c8d2d89bd9bbff4a24df74b36ab
MegaXmas <noahmendelsohn96@gmail.com>
2026-01-05 17:23:04 -0800

    merquee review css for alignment nd media flex updated. not fully finished

--- src/app/pipes/indent-after-first.pipe.ts
+++ src/app/pipes/indent-after-first.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class IndentAfterFirstPipe implements PipeTransform {
     transform(value: string): string {
-        const firstLineIndented = `&#xa0 ` + value;
+        const firstLineIndented = `&#32` + value;
         return firstLineIndented.replaceAll(`\n`, `\n&#xa0&#xa0&#xa0 `);
     }
 }
